a3c682104d79ecfd4978b71a806f5327932c1df7
Felipe Feitosa <felipe.feiitosa08@gmail.com>
2025-10-07 11:39:43 -0300

    Clinica de Psicologia

---
+++ ClinicaPsicologia/ConsultaCasal.java
@@ -0,0 +1,28 @@
+public class ConsultaCasal extends Consulta {
+    private String conflitoPrincipal;
+
+    public ConsultaCasal(String paciente, String psicologo, double valor, String conflitoPrincipal) {
+        super(paciente, psicologo, valor);
+        this.conflitoPrincipal = conflitoPrincipal;
+    }
+    public String getConflitoPrincipal() {
+        return conflitoPrincipal;
+    }
+    public void setConflitoPrincipal(String conflitoPrincipal) {
+        this.conflitoPrincipal = conflitoPrincipal;
+    }
+    public void mostrarDados() {
+        super.mostrarDados();
+        System.out.println("Conflito Principal: " + conflitoPrincipal);
+    }
+    public void mostrarDados(boolean confidencial) {
+        if(confidencial) {
+            mostrarDados();
+            System.out.println("Informações confidenciais não serão exibidas.");
+        }
+        else {
+            mostrarDados();
+            System.out.println("Conflito Principal: " + conflitoPrincipal);
+        }
+    }
+}
